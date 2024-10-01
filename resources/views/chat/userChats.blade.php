b57c92b5e64c09a3088542d3a444d945b3d08231
Bartek Sobczak <bartsob08@gmail.com>
2024-10-01 13:00:59 +0200

    #32 usuniete duplikaty w css

--- resources/views/chat/userChats.blade.php
+++ resources/views/chat/userChats.blade.php
@@ -12,12 +12,13 @@
             </select>
         </div>
     </div>
-    <div class="chat-container" style="overflow-x: hidden;">
+    <div class="chat-container">
         <ul class="list-group mt-3" id="chatList">
             <!-- Lista czatów będzie ładowana dynamicznie za pomocą JavaScript -->
         </ul>
     </div>
 </div>
+
 
 <!-- New Chat Modal -->
 <div id="newChatModal" class="modal" tabindex="-1" role="dialog" style="display:none;">
