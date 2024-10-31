ab69d3b1b7f4be8759708b3e7495c7d41039ea37
Bartek <bartsob08@gmail.com>
2024-10-31 13:04:58 +0100

    removed unused file

--- resources/views/partials/category-node-checkbox.blade.php
+++
@@ -1,7 +0,0 @@
-<div style="max-height: 300px; overflow-y: auto;">
-    <ul>
-        @foreach ($categories as $category)
-        @include('partials.category-node-checkbox-item', ['category' => $category, 'selectedCategories' => $selectedCategories ?? []])
-        @endforeach
-    </ul>
-</div>
