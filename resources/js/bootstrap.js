4ec09f7006c867c71b045bb305d3c73f29bcbb0e
Bartek Sobczak <bartsob08@gmail.com>
2024-12-02 12:32:39 +0100

    #46 konfiguracja reverba - do poprawienia nie dziala

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,34 +1,19 @@
-import 'bootstrap';
+import "bootstrap";
 
 /**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
+ * Ładowanie axios do obsługi żądań HTTP
  */
-
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
+ * Konfiguracja Laravel Echo do korzystania z Reverb jako broadcastera
  */
+import Echo from "laravel-echo";
 
-// import Echo from 'laravel-echo';
-
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+window.Echo = new Echo({
+    broadcaster: "reverb",
+    host: `${window.location.hostname}:6001`, // Port 6001 musi odpowiadać Twojemu Reverb App Port
+});
